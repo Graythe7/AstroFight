bc343acf63ad2c1873496d965440c34bff989d8b
Graythe7 <109918660+Graythe7@users.noreply.github.com>
2025-03-26 15:22:02 +0330

    intro Cutscene Scene animation and transition completed

---
+++ Assets/Scripts/CutScene/introScene.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introScene : MonoBehaviour
+{
+    public float transitionTime;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeScene(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator ChangeScene(int levelIndex)
+    {
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(levelIndex);
+    }
+}
